2ff5826351de0a2e425e453c2677a590ebd4e1a1
Georgi Kardzhaliyski <113539884+georgikdz@users.noreply.github.com>
2026-02-12 23:15:23 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -63,8 +63,7 @@ export default function Home() {
               
               <div className="about-item about-item-highlight">
                 <p className="about-text">
-                  When I'm not working, you'll find me training for my next marathon. I'm pursuing the <strong>Abbott World Marathon Majors</strong> -- a series of the world's 7 most prestigious marathons. So far, I've completed 3/7 Majors: <strong>Chicago</strong> (2019), <strong>Berlin</strong> (2021), and <strong>New York City</strong> (2022), plus six other marathons along the way. <strong>Sydney, London, Tokyo, and Boston </strong> are next on the list .
-                </p>
+When I'm not working, you'll find me training for my next marathon. I'm pursuing the Abbott World Marathon Majors—a series of the world's 7 most prestigious marathons. So far, I've completed 3/7: Chicago (2019), Berlin (2021), and New York City (2022), plus six other marathons along the way. Still to come: Sydney, Tokyo, Boston, and London. </p>
               </div>
             </div>
           </div>
